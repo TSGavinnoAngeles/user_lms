530930b4e2d1e1ca101b5785acb3b1c6bf8e494c
TSGavinnoAngeles <gavinno.angeles@trajectorservices.com>
2024-06-26 01:18:12 +0800

    attempt 1

--- auth.ts
+++ auth.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./app/lib/mongoAda";
-import { revalidatePath } from "next/cache";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: MongoDBAdapter(clientPromise),
@@ -13,20 +12,15 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       return true;
     },
 
-    async jwt({ token, account }) {
-      // if (account) {
-      //   console.log(account.provider);
-      // } else {
-      //   console.log("no account");
-      // }
-      // console.log({ token });
+    async jwt({ token }) {
+      // console.log("JWT", token);
       return token;
     },
 
-    // async session({ session }: any) {
-    //   console.log(session.provider);
-    //   return session;
-    // },
+    async session({ session }: any) {
+      // console.log("Sesh", session);
+      return session;
+    },
   },
 
   ...authConfig,
