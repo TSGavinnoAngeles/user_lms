8d070e2d65ebc23ea9cce28edcecc7183bf8bc39
TSGavinnoAngeles <gavinno.angeles@trajectorservices.com>
2024-07-05 21:37:28 +0800

    attempt to shorten calls by using Promise.all

--- app/api/stripe/[tier]/checkout/route.ts
+++ app/api/stripe/[tier]/checkout/route.ts
@@ -11,21 +11,19 @@ export async function POST(
   req: Request,
   { params }: { params: { tier: string } }
 ) {
-  console.log(`just got in here. ${params.tier}`);
   const tier = params.tier;
   const session = await auth();
+  if (!session) {
+    return NextResponse.redirect("https://user-lms.vercel.app/login");
+  }
+  const sesh = session.user;
+
   try {
-    if (!session) {
-      return NextResponse.redirect("https://user-lms.vercel.app/login");
-    }
-    const sesh = session.user;
+    const [subbingUser, tierDetails] = await Promise.all([
+      User.findOne({ email: sesh?.email, name: sesh?.name }),
+      Price.findOne({ tier: tier }),
+    ]);
 
-    const subbingUser = await User.findOne({
-      email: sesh?.email,
-      name: sesh?.name,
-    });
-
-    const tierDetails = await Price.findOne({ tier: tier });
     if (!tierDetails) {
       return new NextResponse(JSON.stringify({ error: "Tier not found" }), {
         status: 404,
@@ -33,66 +31,70 @@ export async function POST(
     }
 
     if (subbingUser.tier === tierDetails.correspondingTier) {
-      // Assuming you want to redirect to a "/home" page
       return NextResponse.redirect("/catalog");
     }
 
-    let userId = subbingUser._id.toString();
     let userSubcription = await Subscriptions.findOne({ user: subbingUser });
 
     if (!userSubcription) {
       const customer = await stripe.customers.create({
         email: subbingUser.email,
       });
-
       await Subscriptions.create({
         user: subbingUser._id.toString(),
         stripeCustomerId: customer.id,
         amount: "0000",
         paidFor: "nothing",
       });
-
-      userId = subbingUser._id.toString();
     }
 
-    if (userSubcription && userSubcription.stripeSubscriptionId) {
-      const stripeSession = await stripe.billingPortal.sessions.create({
-        customer: userSubcription.stripeCustomerId,
-        return_url: redirectURL,
-      });
+    const stripeSession = await createStripeSession(
+      userSubcription,
+      subbingUser,
+      tierDetails,
+      redirectURL
+    );
 
-      return new Response(JSON.stringify({ url: stripeSession.url }));
-    }
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+  } catch (error: any) {
+    console.error("[STRIPE ERROR]", error.message);
+    return NextResponse.json(`${error}`, { status: 500 });
+  }
+}
 
-    const stripeSession = await stripe.checkout.sessions.create({
+async function createStripeSession(
+  userSubcription: { stripeSubscriptionId: string; stripeCustomerId: string },
+  subbingUser: { email: string; _id: { toString: () => string } },
+  tierDetails: { tier: string; price: string; correspondingTier: string },
+  redirectURL: string
+) {
+  if (userSubcription && userSubcription.stripeSubscriptionId) {
+    return await stripe.billingPortal.sessions.create({
+      customer: userSubcription.stripeCustomerId,
+      return_url: redirectURL,
+    });
+  } else {
+    return await stripe.checkout.sessions.create({
       mode: "payment",
       billing_address_collection: "auto",
       customer_email: subbingUser.email,
       success_url: `https://user-lms.vercel.app/pricing`,
-
       cancel_url: `https://user-lms.vercel.app/catalog`,
       line_items: [
         {
           price_data: {
             currency: "usd",
-            product_data: {
-              name: `${tierDetails.tier} `,
-            },
+            product_data: { name: `${tierDetails.tier} ` },
             unit_amount: Number(tierDetails.price),
           },
           quantity: 1,
         },
       ],
       metadata: {
-        userId,
+        userId: subbingUser._id.toString(),
         amount: `${tierDetails.price}`,
         upgradeTo: `${tierDetails.correspondingTier}`,
       },
     });
-
-    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
-  } catch (error: any) {
-    console.error("[STRIPE ERROR]", error.message);
-    return NextResponse.json(`${error}`, { status: 500 });
   }
 }
